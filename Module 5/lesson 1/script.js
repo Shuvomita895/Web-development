94541ca7bc2069a688df9af1ebd6657ff250f8cb
Shuvomita895 <royshuvomita895@gmail.com>
2026-01-05 20:56:30 +0600

    lesson 1. activity.2

---
+++ Module 5/lesson 1/script.js
@@ -0,0 +1,21 @@
+function myFunction() {
+     var greeting;
+     var time = new Date().getHours();
+
+     if (time < 10){
+         greeting = "Good morning.";
+     }
+        
+    else if (time < 20){
+         greeting = "Good day";
+    }
+       
+    else {
+        greeting = "Good evening";
+    }
+    document.getElementById("time").innerHTML = 
+    "The current time is " + time;
+    document.getElementById("greet").innerHTML =
+    greeting;
+
+}
